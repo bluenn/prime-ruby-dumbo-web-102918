6c99a598b269e737d32b7faa3f5404362bebf896
a lan <bluenowte97@gmail.com>
2018-11-05 19:54:07 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime(number)
+  range = [*1..number]
+
+  for item in range
+    if (number % item == 0 && item != 1 && item != number)
+      false
+     end
+  end   
+
+  true
+end    
