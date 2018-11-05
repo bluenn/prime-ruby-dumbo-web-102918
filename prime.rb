4e72b44993595620243d0f407cc146d26e7bac87
a lan <bluenowte97@gmail.com>
2018-11-05 20:32:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+# 1 is not prime as prime has two divisors, itself and 1
+if number <= 1
+  return false
+end
+
+  range = [*1..number]
+
+  range[1..-2].each do |item|
+    if number % item == 0
+      return false
+     end
+  end
+  true
+end
