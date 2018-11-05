66f27f67476d10f9e38202ed7c4de1e4fa8f1aef
a lan <bluenowte97@gmail.com>
2018-11-05 20:07:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -8,7 +8,7 @@ end
   range = [*1..number]
 
   for item in range
-    if (number % item == 0 && item != 1 && item != number)
+    if (number % item == 0 && item != 1 || item != number)
       false
      end
   end
