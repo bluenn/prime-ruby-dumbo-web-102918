4119a8a0f5d8099e08a46b48d8312c2dc1b12450
a lan <bluenowte97@gmail.com>
2018-11-05 19:58:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(number)
   absolute_num = number.abs
-  range = [*1..absolute]
+  range = [*1..absolute_num]
 
   for item in range
     if (absolute_num % item == 0 && item != 1 && item != absolute_num)
