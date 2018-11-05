5ecae931de392fb8fc898371623172f768197143
a lan <bluenowte97@gmail.com>
2018-11-05 19:59:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
-def prime?(number)
-  absolute_num = number.abs
-  range = [*1..absolute_num]
+def prime?(number > 0)
+  
+  range = [*1..number]
 
   for item in range
-    if (absolute_num % item == 0 && item != 1 && item != absolute_num)
+    if (number % item == 0 && item != 1 && item != number)
       false
      end
   end
